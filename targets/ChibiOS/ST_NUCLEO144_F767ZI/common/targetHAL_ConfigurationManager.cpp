a8bff8d457b4ecb8271574f16614fdd500f090c8
Robin Jones <networkfusion@users.noreply.github.com>
2023-09-29 18:28:31 +0100

    Test mac manipulation

--- targets/ChibiOS/ST_NUCLEO144_F767ZI/common/targetHAL_ConfigurationManager.cpp
+++ targets/ChibiOS/ST_NUCLEO144_F767ZI/common/targetHAL_ConfigurationManager.cpp
@@ -31,22 +31,23 @@ bool InitialiseNetworkDefaultConfig(HAL_Configuration_NetworkInterface *pconfig,
     pconfig->AutomaticDNS = 1;
     pconfig->SpecificConfigId = UINT32_MAX;
 
-// #ifdef USE_ST_DEV_MAC
-//     // set MAC address with ST provided MAC for development boards
-//     // 00:80:E1:01:35:D1
-//     pconfig->MacAddress[0] = 0x00;
-//     pconfig->MacAddress[1] = 0x80;
-//     pconfig->MacAddress[2] = 0xE1;
-//     pconfig->MacAddress[3] = 0x01;
-//     pconfig->MacAddress[4] = 0x35;
-//     pconfig->MacAddress[5] = 0xD1;
-// #else
-    pconfig->MacAddress[0] = 0x02; // *(uint32_t*)DEVICE_ID1_ADDR >> 16; // FIXME: second byte must be 2,6,A,E
+#ifdef USE_ST_DEV_MAC
+    // set MAC address with ST provided MAC for development boards
+    // 00:80:E1:01:35:D1
+    pconfig->MacAddress[0] = 0x00;
+    pconfig->MacAddress[1] = 0x80;
+    pconfig->MacAddress[2] = 0xE1;
+    pconfig->MacAddress[3] = 0x01;
+    pconfig->MacAddress[4] = 0x35;
+    pconfig->MacAddress[5] = 0xD1;
+#else
+    // Set a locally administered MAC
+    pconfig->MacAddress[0] = *(uint32_t*)DEVICE_ID1_ADDR >> 16 | 0x02; // second byte must be 2,6,A,E
     pconfig->MacAddress[1] = *(uint32_t*)DEVICE_ID1_ADDR & 0x0000FFFFuL;
     pconfig->MacAddress[2] = *(uint32_t*)DEVICE_ID2_ADDR >> 16;
     pconfig->MacAddress[3] = *(uint32_t*)DEVICE_ID2_ADDR & 0x0000FFFFuL;
     pconfig->MacAddress[4] = *(uint32_t*)DEVICE_ID3_ADDR >> 16;
     pconfig->MacAddress[5] = *(uint32_t*)DEVICE_ID3_ADDR & 0x0000FFFFuL;
-// #endif
+#endif
     return true;
 }
